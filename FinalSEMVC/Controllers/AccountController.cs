c74f9974cfcfba220729a6b232499b7bae21510f
ShenLong6606 <longle19001560@gmail.com>
2023-05-09 01:31:11 +0700

    Add project files.

---
+++ FinalSEMVC/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FinalSEMVC.Models;
+
+namespace FinalSEMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        //Get: Account
+        [HttpGet]
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        void connectionString()
+        {
+            con.ConnectionString = "data source=(local)\\SQLEXPRESS; database=Login;integrated security = SSPI;";
+            ;
+        }
+        [HttpPost]
+        public ActionResult Verify(Account acc)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from users where Username ='" + acc.Name + "' and Password ='" + acc.Password + "'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return View("Create");
+            }
+            else
+            {
+                con.Close();
+                return View("error");
+            }
+
+
+        }
+    }
+}
---
+++ FinalSEMVC/Controllers/BillsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FinalSEMVC.Models;
+
+namespace FinalSEMVC.Controllers
+{
+    public class BillsController : Controller
+    {
+        private Shop1Entities db = new Shop1Entities();
+
+        // GET: Bills
+        public ActionResult Index()
+        {
+            return View(db.Bills.ToList());
+        }
+
+        // GET: Bills/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bill bill = db.Bills.Find(id);
+            if (bill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bill);
+        }
+
+        // GET: Bills/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Bills/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BillID,AgentID,Price,Deliver,Pay")] Bill bill)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Bills.Add(bill);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bill);
+        }
+
+        // GET: Bills/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bill bill = db.Bills.Find(id);
+            if (bill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bill);
+        }
+
+        // POST: Bills/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BillID,AgentID,Price,Deliver,Pay")] Bill bill)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bill).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bill);
+        }
+
+        // GET: Bills/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bill bill = db.Bills.Find(id);
+            if (bill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bill);
+        }
+
+        // POST: Bills/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Bill bill = db.Bills.Find(id);
+            db.Bills.Remove(bill);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
